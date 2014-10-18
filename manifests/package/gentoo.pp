b48dd2cbc093e03831d08094a9d25b648d23b5ed
Eric Gisse <eric.gisse@gmail.com>
2014-10-18 10:56:04 +0000

    Gentoo support, tested working

--- manifests/package/gentoo.pp
+++ manifests/package/gentoo.pp
@@ -1,6 +1,18 @@
 # Class: nginx::package::gentoo
 #
-# Manage the nginx package on Gentoo
+# This module manages NGINX package installation on Gentoo based systems
+#
+# Parameters:
+#
+# There are no default parameters for this class.
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# This class file is not called directly
 class nginx::package::gentoo(
     $package_name   = 'www-servers/nginx',
     $package_ensure = 'present'
